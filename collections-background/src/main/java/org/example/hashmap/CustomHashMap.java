09b2d2f7354c519cf082e4a9816cfe72ea56e303
Murad Teymurov <murad.teymurov@sinam.net>
2023-12-25 00:55:24 +0400

    improve hashmap

--- collections-background/src/main/java/org/example/hashmap/CustomHashMap.java
+++ collections-background/src/main/java/org/example/hashmap/CustomHashMap.java
@@ -1,14 +1,19 @@
 package org.example.hashmap;
 
-public class CustomHashMap <K,V>{
+public class CustomHashMap<K, V> {
 
     private int size;
     private int capacity;
-    private Node<K,V>[] buckets;
+    private Node<K, V>[] buckets;
+
+    private float loadFactor;
+    private int threshold;
 
     public CustomHashMap() {
         this.capacity = 16;
         this.buckets = new Node[capacity];
+        this.loadFactor = 0.75f;
+        this.threshold = (int) (capacity * loadFactor);
     }
 
     public int getSize() {
@@ -16,23 +21,29 @@ public class CustomHashMap <K,V>{
     }
 
     public void put(K key, V value) {
-        Node<K,V> newNode = new Node<>(key, value, null);
+        Node<K, V> newNode = new Node<>(key, value, null);
+
+        if (size == threshold) {
+            increaseCapacity();
+            resizeBuckets();
+        }
+
         int index = getIndex(key);
 
-        Node<K,V> currentNode = buckets[index];
+        Node<K, V> currentNode = buckets[index];
 
-        if(currentNode == null) {
+        if (currentNode == null) {
             buckets[index] = newNode;
             size++;
         } else {
-            while (currentNode.next!= null) {
-                if(currentNode.key.equals(key)){
+            while (currentNode.next != null) {
+                if (currentNode.key.equals(key)) {
                     currentNode.value = value;
                     return;
                 }
                 currentNode = currentNode.next;
             }
-            if(currentNode.key.equals(key)) {
+            if (currentNode.key.equals(key)) {
                 currentNode.value = value;
             } else {
                 currentNode.next = newNode;
@@ -43,10 +54,10 @@ public class CustomHashMap <K,V>{
 
     public V get(K key) {
         int index = getIndex(key);
-        Node<K,V> currentNode = buckets[index];
+        Node<K, V> currentNode = buckets[index];
 
-        while(currentNode != null) {
-            if(currentNode.key.equals(key)) {
+        while (currentNode != null) {
+            if (currentNode.key.equals(key)) {
                 return currentNode.value;
             }
             currentNode = currentNode.next;
@@ -56,22 +67,58 @@ public class CustomHashMap <K,V>{
 
     private int getIndex(K key) {
         int hashCode = key.hashCode();
-        int index = Math.abs(hashCode & (capacity-1)); // hashcode maybe negative
+        int index = Math.abs(hashCode & (capacity - 1)); // hashcode maybe negative
         return index;
     }
 
+    private void increaseCapacity() {
+        capacity *= 2;
+        threshold *= 2;
+    }
 
-    private class Node<K,V> {
+    private void resizeBuckets() {
+        Node<K, V>[] newBuckets = new Node[capacity];
+        for (int i = 0; i < buckets.length; i++) {
+//            newBuckets[i] = buckets[i]; not enough!!!
+            Node<K, V> node = buckets[i];
+            if (node != null) {
+                int index = getIndex(node.key);
+                Node<K, V> current = buckets[index];
+                if (current == null) {
+                    newBuckets[index] = node;
+                } else {
+                    while (current.next != null) {
+                        current = current.next;
+                    }
+                    current.next = node;
+                }
+            }
+        }
+        this.buckets = newBuckets;
+    }
+
+
+    private class Node<K, V> {
         private int hashCode;
         private K key;
         private V value;
-        private Node<K,V> next;
+        private Node<K, V> next;
 
         public Node(K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
             this.hashCode = key.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "hashCode=" + hashCode +
+                    ", key=" + key +
+                    ", value=" + value +
+                    ", next=" + next +
+                    '}';
         }
     }
 }
